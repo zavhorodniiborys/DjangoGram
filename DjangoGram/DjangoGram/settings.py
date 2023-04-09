7a6be44b13f889a0dc373592f9a8af39948b1051
Zavhorodnii Boris <zavgorodnijboris188@gmail.com>
2023-04-09 18:49:27 +0300

    Docker is ready

--- DjangoGram/DjangoGram/settings.py
+++ DjangoGram/DjangoGram/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import environ
 # Initialise environment variables
 env = environ.Env()
-environ.Env.read_env()
+env.read_env('env/dj_gram.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,28 +84,24 @@ WSGI_APPLICATION = 'DjangoGram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env('DB_NAME'),
-#         'USER': env('DB_USER'),
-#         'PASSWORD': env('DB_PASSWORD'),
-#         'HOST': env('DB_HOST'),
-#         'PORT': '5432',
-        # 'TEST': {
-        #     'ENGINE': 'django.db.backends.sqlite3',
-        #     'NAME': env('DB_NAME'),
-        # }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
